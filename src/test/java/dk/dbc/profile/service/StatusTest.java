de63a46f34e4f1d2b10bbc9502b6c109d344fc15
Morten Bøgeskov <mb@dbc.dk>
2019-03-18 09:42:02 +0100

    Dummy Test

---
+++ src/test/java/dk/dbc/profile/service/StatusTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2019 DBC A/S (http://dbc.dk/)
+ *
+ * This is part of profile-service
+ *
+ * profile-service is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * profile-service is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dk.dbc.profile.service;
+
+import org.junit.Test;
+
+
+/**
+ *
+ * @author Morten Bøgeskov (mb@dbc.dk)
+ */
+public class StatusTest {
+
+    public StatusTest() {
+    }
+
+    @Test(timeout = 2_000L)
+    public void dummy() throws Exception {
+        System.out.println("dummy");
+    }
+}
